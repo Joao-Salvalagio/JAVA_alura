6244e6ed8ad22a0c1283a96bcd7f68c6fb1c8c59
Joao-Salvalagio <joaosalvalagio@gmail.com>
2025-09-08 12:17:59 -0300

    Aula 01 concluida com sucesso!

--- src/JAVA_criando_sua_primeira_aplicacao/Aula01.java
+++ src/JAVA_criando_sua_primeira_aplicacao/Aula01.java
@@ -1,6 +1,6 @@
 package JAVA_criando_sua_primeira_aplicacao;
 
-public class Main {
+public class Aula01 {
     public static void main(String[] args) {
         System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
     }
