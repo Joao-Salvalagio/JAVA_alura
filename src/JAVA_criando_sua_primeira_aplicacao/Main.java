7c1d1605f70cd7adf9f8baded1e8ae03aef1dc89
Joao-Salvalagio <joaosalvalagio@gmail.com>
2025-09-08 12:16:48 -0300

    "Att"

--- src/JAVA_criando_sua_primeira_aplicacao/Main.java
+++ src/JAVA_criando_sua_primeira_aplicacao/Main.java
@@ -1,4 +1,7 @@
 package JAVA_criando_sua_primeira_aplicacao;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+    }
 }
